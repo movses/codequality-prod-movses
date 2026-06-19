a23c592522b57884f41dd8aff51e988be159c4be
movses <movses.saponjyan@gmail.com>
2026-06-19 15:05:22 +0400

    b

--- server.js
+++
@@ -1,8 +0,0 @@
-/*
-*  This code is calculating total price
-*/
-
-export function processOrder(order) {
-  const total = order.price * order.quantity;
-  return total;
-}
--- sonar.py
+++
@@ -1,4 +0,0 @@
-# SONAR detection module
-def detect_sonar_objects(signal):
-    # Process sonar signal
-    return signal * 0.5
