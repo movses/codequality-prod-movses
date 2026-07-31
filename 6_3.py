b0c7998e0aa34adcc4c4b622010f96833fb37bb9
movses <movses.saponjyan@gmail.com>
2026-07-31 15:25:22 +0400

    3

--- 6_3.py
+++ 6_3.py
@@ -1,13 +1,1 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route("/calc")
-def calc():
-    expr = request.args.get("expr")
-    return str(eval(expr))
-
-if __name__ == "__main__":
-    app.run()
-
 x = 1
